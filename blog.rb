c34868b854ecd90316ed72d0170f2c5ebf75fd3d
Manali <manalijoshi82@gmail.com>
2015-02-04 21:37:08 -0800

    manali checks in

--- blog.rb
+++ blog.rb
@@ -1,3 +1,5 @@
 class Blogpost
 	attr_accessor :title, :all_blog_posts, :total_blog_posts
+
+	def initialize
 end
--- second-file.rb
+++ second-file.rb
@@ -2,3 +2,4 @@ puts "Just some more branching practice"
 
 puts "and some more branching and master"
 
+puts " changing to tackle the merge conflict"	
